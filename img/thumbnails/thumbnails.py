c8385f497ce11aa5512a0cc5e236302c3a0d5817
LinusAlteborn <118182579+LinusAlteborn@users.noreply.github.com>
2023-03-27 16:37:25 +0200

    Tummnaggels klippling

--- img/thumbnails/thumbnails.py
+++ img/thumbnails/thumbnails.py
@@ -1,5 +1,8 @@
-// Data för alla mina spel // WARNING, den är massiv
-var games = [
+import requests
+from PIL import Image
+import os
+
+games = [
     {
         "name": "8 Ball Pool (Update) New difficulty",
         "created": "2019-12-15T10:02:57.000Z",
@@ -434,170 +437,16 @@ var games = [
     }
 ]
 
-function time_since(date) {
-    const seconds = Math.floor((new Date() - new Date(date)) / 1000);
-    const intervals = [
-        { label: 'year',   seconds: 31536000 },
-        { label: 'month',  seconds: 2592000 },
-        { label: 'week',   seconds: 604800 },
-        { label: 'day',    seconds: 86400 },
-        { label: 'hour',   seconds: 3600 },
-        { label: 'minute', seconds: 60 },
-        { label: 'second', seconds: 1 }
-    ];
-    
-    for (let i = 0; i < intervals.length; i++) {
-        const interval = intervals[i];
-        const count = Math.floor(seconds / interval.seconds);
-        if (count >= 1) {
-            return count + ' ' + interval.label + (count > 1 ? 's' : '') + ' ago';
-        }
-    }
-    
-    return 'just now';
-}
+def fetch():
+    for id, game in enumerate(games):
+        link = game["images"]["big"]
+        img_data = requests.get(link).content
+        with open(str(id) + '.png', 'wb') as handler:
+            handler.write(img_data)
+        image = Image.open(str(id) + '.png')
+        image = image.convert('RGB')
+        image.save(str(id) + '.webp', 'webp')
+        os.remove(str(id) + ".png")
+        print(link)
 
-// Mina html templates som jag placerar i html sidor (jag använder semikolon för att kunna gömma dem templates jag inte använder)
-var templates ={
-    "header":(
-    '<nav id="nav-links">' +
-    '   <h2><a href="about.html">About</a></h2>' +
-    '   <h2><a href="#">Contact</a></h2>' +
-    '   <h2><a href="#">Forum</a></h2>' +
-    '</nav>' +
-    '<div id="burger">' +
-    '   <div></div>' +
-    '   <div></div>' +
-    '   <div></div>' +
-    '</div>' +
-    '<h1><a href="index.html"><img src="img/icons/logo.png" alt="Binus Spelus"></a></h1>' +
-    '<a href="login.html" id="nav-links-login"><img src="img/icons/account.svg" alt="login"></a>'
-    ),
-
-    "footer":(
-    '<hr>' +
-    '<p>Binus© 2023</p>'
-    ),
-
-    "game-link":(
-        `<a href="game.html?-1" class="game">
-            <img src="" alt="Thumbnail">
-            <h2>Game name</h2>
-            <p>just now</p>
-        </a>`
-    )
-}
-
-// Funktioner som byter innehåll på header of footer element på sidan
-function load_header_and_footer(){
-    document.getElementsByTagName("header")[0].innerHTML = templates.header;
-    document.getElementsByTagName("footer")[0].innerHTML = templates.footer;
-    document.getElementById('burger').addEventListener('click', burger_menu);
-}
-
-// Här är alla funktioner som ändrar sidan när den startar (ändrar spel texter, ändrar användarnamns texter osv)
-function load_site(name){
-    // header and footer ska laddas in på alla sidor
-    load_header_and_footer()
-
-    if (name == "" || name == "index"){
-        render_games();
-        document.getElementById("load-more").addEventListener('click', function(){load_more_games(8);});
-    }
-    if (name === "game"){
-        document.getElementById("game").src = "games/" + game_id + "/index.html";
-        document.getElementById("game_info_name").innerHTML = games[game_id].name;
-        document.getElementById("game_info_engine").innerHTML = "Made in Scratch " + time_since(games[game_id].shared);
-    }
-    if (name === "login" || name === "register"){
-        form.addEventListener('submit', handleForm);
-        eye.addEventListener('click', showPassword);
-    }
-    if (name === "account"){
-        document.getElementById("account-name").innerHTML = sessionStorage.getItem('username');
-    }
-    // visar din profil bild och byter länk till account sidan om du är inloggad
-    if (sessionStorage.getItem('loggedin') == "true"){
-        document.getElementById("nav-links-login").children[0].src = "img/profile/pfp.png";
-        document.getElementById("nav-links-login").href = "account.html";
-        document.getElementById("nav-links-login").id = "bruh";
-    }
-}
-
-function burger_menu(){
-    document.getElementById('burger').classList.toggle('burger-toggle');
-    document.getElementById('nav-links').classList.toggle('nav-active');
-}
-
-function add_game(game_id){
-    // lägg till spel objektet i binder
-    document.getElementById("binder").innerHTML += templates["game-link"];
-}
-
-function render_game(element, id) {
-    element.href = "game.html?" + id;
-    element.getElementsByTagName("h2")[0].innerHTML = games[id].name;
-    element.getElementsByTagName("img")[0].src = "img/thumbnails/" + id + ".webp";
-    element.getElementsByTagName("p")[0].innerHTML = time_since(games[id].shared);
-}
-
-function render_games() {
-    for(var i = 0; i < document.getElementById("binder").childElementCount; i++) {
-        render_game(document.getElementById("binder").children[i], i);
-    }
-}
-
-function load_more_games(amount){
-    var start = document.getElementById("binder").childElementCount;
-    for(var i = start; i < start + amount; i++){
-        add_game(i);
-        if (i >= games.length - 1){
-            document.getElementById("load-more").remove();
-            return
-        }
-    }
-    render_games();
-}
-
-function handleForm(event) {
-    event.preventDefault();
-    submitter = event.submitter.value;
-    if (submitter === "login"){ login(); }
-    if (submitter === "register"){ register(); }
-}
-
-function login() {
-    if (usernameField.value == sessionStorage.getItem('username') && passwordField.value == sessionStorage.getItem('password')){
-        sessionStorage.setItem('loggedin', true);
-        window.location.assign('account.html');
-        return
-    }
-    logout();
-}
-function logout(){
-    sessionStorage.setItem('loggedin', false);
-    window.location.assign('login.html');
-}
-function register(){
-    sessionStorage.setItem('username', usernameField.value);
-    sessionStorage.setItem('password', passwordField.value);
-    window.location.assign('login.html');
-}
-function showPassword() {
-    if (passwordField.type == "password") {
-        passwordField.type = "text";
-        eye.src = "img/icons/hide.svg";
-    } else {
-        passwordField.type = "password";
-        eye.src = "img/icons/view.svg";
-    }
-}
-
-const usernameField = document.getElementById("username-field")
-const passwordField = document.getElementById("password-field")
-const eye = document.getElementsByClassName("eye")[0]
-const form = document.getElementsByTagName("form")[0]
-const site_name = window.location.pathname.split("/").pop().split(".")[0]
-const game_id = location.search.substring(1)
-// laddar in alla element som genereras genom javascript
-window.onload = function (){load_site(site_name)}
+fetch()
